f6b689bd5af597a49fb74bc6234c487eb2f347c2
ahezard <ahezard@gmail.com>
2016-09-12 00:57:48 +0200

    Add support for .argv file and more option in .ini file
    NTR ARM7 switch not yet implemented

--- arm9/source/bootstrap.cpp
+++ arm9/source/bootstrap.cpp
@@ -20,6 +20,7 @@
 
 #include <nds.h>
 #include <fat.h>
+#include <limits.h>
 
 #include <stdio.h>
 
@@ -59,8 +60,55 @@ void WaitForSlot() {
 	}
 }
 */
+void runFile(string filename) {
+	vector<char*> argarray;
+	
+	if ( strcasecmp (filename.c_str() + filename.size() - 5, ".argv") == 0) {
+		FILE *argfile = fopen(filename.c_str(),"rb");
+		char str[PATH_MAX], *pstr;
+		const char seps[]= "\n\r\t ";
+
+		while( fgets(str, PATH_MAX, argfile) ) {
+			// Find comment and end string there
+			if( (pstr = strchr(str, '#')) )
+				*pstr= '\0';
+
+			// Tokenize arguments
+			pstr= strtok(str, seps);
+
+			while( pstr != NULL ) {
+				argarray.push_back(strdup(pstr));
+				pstr= strtok(NULL, seps);
+			}
+		}
+		fclose(argfile);
+		filename = argarray.at(0);
+	} else {
+		argarray.push_back(strdup(filename.c_str()));
+	}
+
+	if ( strcasecmp (filename.c_str() + filename.size() - 4, ".nds") != 0 || argarray.size() == 0 ) {
+		iprintf("no nds file specified\n");
+	} else {
+		iprintf ("Running %s with %d parameters\n", argarray[0], argarray.size());
+		int err = runNdsFile (argarray[0], argarray.size(), (const char **)&argarray[0]);
+		iprintf ("Start failed. Error %i\n", err);
+
+	}
+}
 
 int main( int argc, char **argv) {
+	bool ntrMode = false;
+	if (argc >= 2) {
+		if ( strcasecmp (argv[1], "NTR") == 0 ) {
+			ntrMode = true;
+		}		
+	}
+	
+	if(ntrMode) {
+		REG_SCFG_CLK = 0x80;
+		REG_SCFG_EXT = 0x83000000; // NAND/SD Access
+	}
 	
 	//unsigned int * SCFG_MC=(unsigned int*)0x4004010;
 	
@@ -71,12 +119,55 @@ int main( int argc, char **argv) {
 	
 	// WaitForSlot();
 	
+	if (0 != argc ) {
+		int i;
+		for (i=0; i<argc; i++ ) {
+			if (argv[i]) printf("[%d] %s\n", i, argv[i]);
+		}
+	} else {
+		printf("No arguments passed!\n");
+	}
+	
+	if(ntrMode) {
+		printf("NTR mode enabled\n");
+	}
+	
 	if (fatInitDefault()) {
 		CIniFile bootstrapini( "sd:/nds-bootstrap.ini" );
 
-		std::string	ndsPath = bootstrapini.GetString( "NDS-BOOTSTRAP", "NDS_PATH", "");
-	
-		runNdsFile(ndsPath.c_str(), 0, NULL);
+		std::string	ndsPath = bootstrapini.GetString( "NDS-BOOTSTRAP", "NDS_PATH", "");	
+
+		if(bootstrapini.GetInt("NDS-BOOTSTRAP","BOOST_CPU",0) == 1) {	
+			printf("CPU boosted\n");
+			REG_SCFG_CLK |= 1;
+		}		
+		
+		if(bootstrapini.GetInt("NDS-BOOTSTRAP","NTR_MODE_SWITCH",0) == 1) {		
+			std::string	bootstrapPath = bootstrapini.GetString( "NDS-BOOTSTRAP", "BOOTSTRAP_PATH", "");
+			
+			// run an argv file
+			
+			printf("NTR MODE SWITCH\n");		
+			
+			if(!ntrMode) {
+				printf("RERUN BOOTSTRAP in NTR mode via argv\n");				
+				printf("Running %s\n", bootstrapPath.c_str());
+				
+				for (int i = 0; i < 120; i++) { swiWaitForVBlank(); }
+				runFile(bootstrapPath.c_str());
+			} else {				
+				printf("Running %s\n", ndsPath.c_str());
+				
+				for (int i = 0; i < 120; i++) { swiWaitForVBlank(); }
+				runFile(ndsPath.c_str());
+			}
+		} else {
+			printf("TWL MODE enabled\n");			
+			printf("Running %s\n", ndsPath.c_str());
+			
+			for (int i = 0; i < 120; i++) { swiWaitForVBlank(); }			
+			runFile(ndsPath.c_str());
+		}		
 	} else {
 		printf("SD init failed!\nLauncher not patched!");
 	}
