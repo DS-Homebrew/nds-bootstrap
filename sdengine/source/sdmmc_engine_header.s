57693f29f3d1abfd3f3063dd676a10352393e706
ahezard <ahezard@gmail.com>
2016-09-20 21:27:58 +0200

    Modifiy the build to have the .s file at the beginning of the bin

--- sdengine/source/sdmmc_engine_header.s
+++ sdengine/source/sdmmc_engine_header.s
@@ -1,20 +1,9 @@
-@   NitroHax -- Cheat tool for the Nintendo DS
-@   Copyright (C) 2008  Michael "Chishm" Chisholm
-@
-@   This program is free software: you can redistribute it and/or modify
-@   it under the terms of the GNU General Public License as published by
-@   the Free Software Foundation, either version 3 of the License, or
-@   (at your option) any later version.
-@
-@   This program is distributed in the hope that it will be useful,
-@   but WITHOUT ANY WARRANTY; without even the implied warranty of
-@   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-@   GNU General Public License for more details.
-@
-@   You should have received a copy of the GNU General Public License
-@   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-.arm
+@---------------------------------------------------------------------------------
+	.section ".init"
+@---------------------------------------------------------------------------------
+	.global _start
+	.align	4
+	.arm
 
 .global sdmmc_engine_start
 .global sdmmc_engine_start_sync
