02608985aec5913666ab0c7d11c1971030d9c085
ahezard <ahezard@gmail.com>
2016-09-22 00:08:28 +0200

    Bootloader start by reducing the size of the dldi driver

--- dldi/source/dldi_header.s
+++ dldi/source/dldi_header.s
@@ -22,6 +22,7 @@
 	.word	0xBF8DA5ED		@ Magic number to identify this region
 	.asciz	" Chishm"		@ Identifying Magic string (8 bytes with null terminator)
 	.byte	0x01			@ Version number
+	.byte	0x0d		@ 8KiB	@ Log [base-2] of the size of this driver in bytes.
 	.byte	FIX_GOT | FIX_BSS | FIX_GLUE | FIX_ALL	@ Sections to fix
 	.byte 	0x00			@ Space allocated in the application, not important here.
 	
