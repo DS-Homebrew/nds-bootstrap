dafa194efc0d3077f074598ca62d5ca6d86bf658
ahezard <ahezard@gmail.com>
2016-09-25 01:10:54 +0200

    Fix typo

--- build_cia.sh
+++ build_cia.sh
@@ -1,7 +1,7 @@
 GAME_TITLE="NDS BOOTSTRAP"
 GAME_SUBTITLE1="Runs an .nds file"
 GAME_SUBTITLE2="made by devkitpro"
-GAME_INFO="KBSE 01 NTRBOOSTRAP"
+GAME_INFO="KBSE 01 NDSBOOTSTRAP"
 
 $DEVKITARM/bin/ndstool	-c bootstrap.nds -7 nds-bootstrap.arm7.elf -9 nds-bootstrap.arm9.elf -g $GAME_INFO -b icon.bmp  "$GAME_TITLE;$GAME_SUBTITLE1;$GAME_SUBTITLE2"  -r9 0x2000000 -r7 0x2380000 -e9 0x2000000 -e7 0x2380000
 cp bootstrap.nds bootstrap-nogba.nds
