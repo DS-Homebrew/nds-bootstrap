917dbde1d0f207aa245f9bdbf08ef173c4e39dc9
ApacheThunder <spenserwhaley@gmail.com>
2016-09-24 18:53:42 -0500

    Fix wrong Arm7 SCFG values for bootstrap-dldi

--- arm7/source/cheat_engine_arm7.c
+++ arm7/source/cheat_engine_arm7.c
@@ -24,9 +24,9 @@ void runCheatEngineCheck (void)
 	{
 
 		REG_SCFG_ROM = 0x703;
-		if(fifoCheckValue32(FIFO_USER_04)) { REG_SCFG_CLK=0x0180; }
-		REG_SCFG_EXT = 0x12A00000;
-		// REG_SCFG_EXT = 0x92A00000;
+		if(fifoCheckValue32(FIFO_USER_04)) { REG_SCFG_CLK=0x0181; }
+		// REG_SCFG_EXT = 0x12A00000;
+		REG_SCFG_EXT = 0x93A40000;
 
 		irqDisable (IRQ_ALL);
 		*((vu32*)0x027FFE34) = (u32)0x06000000;
