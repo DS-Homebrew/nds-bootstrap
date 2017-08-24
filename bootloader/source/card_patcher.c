5522e1fc452749f2459db207c5c40b6ebba73912
Robz8 <bobesh8@gmail.com>
2017-08-24 17:29:08 -0600

    Disable random patch for Harvest Moon DS

--- bootloader/source/card_patcher.c
+++ bootloader/source/card_patcher.c
@@ -508,6 +508,7 @@ u32 patchCardNdsArm9 (const tNDSHeader* ndsHeader, u32* cardEngineLocation, modu
 	
 	if(moduleParams->sdk_version > 0x3000000
 	&& (*(u32*)(0x27FF00C) & 0x00FFFFFF) != 0x544B41	// Doctor Tendo
+	&& (*(u32*)(0x27FF00C) & 0x00FFFFFF) != 0x434241	// Harvest Moon DS
 	&& (*(u32*)(0x27FF00C) & 0x00FFFFFF) != 0x4C5741) // TWEWY
 	{
 		u32 randomPatchOffset =  
