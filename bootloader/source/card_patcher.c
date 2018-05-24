39fcfc95965f0b8947b137cfca5a21801710bb00
Robz8 <bobesh8@gmail.com>
2018-05-23 22:28:45 -0600

    Revert "Use DSi WRAM as cache, for SDK4 games to work without donor ROM"
    This reverts commit 2cb2e97684108f945cc4edffb4ee49a308a4f47e.

--- bootloader/source/card_patcher.c
+++ bootloader/source/card_patcher.c
@@ -119,8 +119,6 @@ u32 mpuInitRegion3Data[1] = {0x8000035};
 
 u32 mpuInitCache[1] = {0xE3A00042};
 
-extern u32 returnedSDKver;
-
 bool cardReadFound = false;
 
 //
@@ -189,7 +187,6 @@ module_params_t* findModuleParams(const tNDSHeader* ndsHeader, u32 donorSdkVer)
 				break;
 		}
 	}
-	returnedSDKver = ((module_params_t*)(moduleparams - 0x1C))->sdk_version;
 	return (module_params_t*)(moduleparams - 0x1C);
 }
 
--- bootloader/source/main.arm7.c
+++ bootloader/source/main.arm7.c
@@ -342,7 +342,6 @@ void loadBinary_ARM7 (aFile file)
 
 u32 enableExceptionHandler = true;
 u32 dsiWramUsed = false;
-u32 returnedSDKver = 0;
 
 void loadRomIntoRam(aFile file) {
 	u32 ROMinRAM = 0;
@@ -368,10 +367,6 @@ void loadRomIntoRam(aFile file) {
 	|| (ROM_TID & 0x00FFFFFF) == 0x323343)	// Ace Attorney Investigations: Miles Edgeworth
 	{
 		dsiWramUsed = true;
-	}
-
-	if (useArm7Donor == 0 && returnedSDKver > 0x4000000) {
-		dsiWramUsed = true;	// Use DSi WRAM as cache, for SDK4 games to work without donor ROM
 	}
 
 	if((romSize & 0x0000000F) == 0x1
