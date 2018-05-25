e25e4d3ee0e3fb3ea10bc2e82891d0de07095f7a
Robz8 <bobesh8@gmail.com>
2018-05-25 11:38:36 -0600

    Make volume adjust code smaller

--- cardengine_arm7/source/cardEngine.c
+++ cardengine_arm7/source/cardEngine.c
@@ -263,99 +263,67 @@ void myIrqHandlerVBlank(void) {
 		u8 i2cVolLevel = i2cReadRegister(0x4A, 0x40);
 		switch(i2cVolLevel) {
 			case 0x00:
+			case 0x01:
 			default:
 				volLevel = 0;
 				break;
-			case 0x01:
-				volLevel = 0;
-				break;
 			case 0x02:
-				volLevel = 1;
-				break;
 			case 0x03:
 				volLevel = 1;
 				break;
 			case 0x04:
-				volLevel = 3;
-				break;
 			case 0x05:
 				volLevel = 3;
 				break;
 			case 0x06:
-				volLevel = 6;
-				break;
 			case 0x07:
 				volLevel = 6;
 				break;
 			case 0x08:
-				volLevel = 10;
-				break;
 			case 0x09:
 				volLevel = 10;
 				break;
 			case 0x0A:
-				volLevel = 15;
-				break;
 			case 0x0B:
 				volLevel = 15;
 				break;
 			case 0x0C:
-				volLevel = 21;
-				break;
 			case 0x0D:
 				volLevel = 21;
 				break;
 			case 0x0E:
-				volLevel = 28;
-				break;
 			case 0x0F:
 				volLevel = 28;
 				break;
 			case 0x10:
-				volLevel = 36;
-				break;
 			case 0x11:
 				volLevel = 36;
 				break;
 			case 0x12:
-				volLevel = 45;
-				break;
 			case 0x13:
 				volLevel = 45;
 				break;
 			case 0x14:
-				volLevel = 55;
-				break;
 			case 0x15:
 				volLevel = 55;
 				break;
 			case 0x16:
-				volLevel = 66;
-				break;
 			case 0x17:
 				volLevel = 66;
 				break;
 			case 0x18:
-				volLevel = 78;
-				break;
 			case 0x19:
 				volLevel = 78;
 				break;
 			case 0x1A:
-				volLevel = 91;
-				break;
 			case 0x1B:
 				volLevel = 91;
 				break;
 			case 0x1C:
-				volLevel = 105;
-				break;
 			case 0x1D:
 				volLevel = 105;
 				break;
 			case 0x1E:
-				volLevel = 120;
-				break;
 			case 0x1F:
 				volLevel = 120;
 				break;
