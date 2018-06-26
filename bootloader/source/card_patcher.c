059834b112f6d7109b539a79238d5a998944345c
Robz8 <bobesh8@gmail.com>
2018-06-25 18:32:21 -0600

    fix compilation error

--- bootloader/source/card_patcher.c
+++ bootloader/source/card_patcher.c
@@ -224,7 +224,7 @@ u32 generateA7Instr(int arg1, int arg2) {
 
 u32 generateA7InstrThumb(int arg1, int arg2) {
     // 23 bit offset
-    u32 offset = ((u32)(arg2 - arg1 - 8);
+    u32 offset = (u32)(arg2 - arg1 - 8);
     // 1st instruction contains the upper 11 bit of the offset
     u16 firstInstr = (( offset >> 13) & 0x7FF) | 0xF000;
 
