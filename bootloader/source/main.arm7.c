d8f9aa33c79a7a10b4083dc4516a65b3fb442e61
Robz8 <bobesh8@gmail.com>
2018-07-01 19:31:52 -0600

    Restore async prefetching (part 2)

--- bootloader/source/main.arm7.c
+++ bootloader/source/main.arm7.c
@@ -817,7 +817,7 @@ void arm7_main (void) {
 
 	setArm9Stuff(*romFile);
 
-	if(romread_LED == 1) {
+	if(romread_LED == 1 || romread_LED == 2) {
 		i2cWriteRegister(0x4A, 0x30, 0x12);    // Turn WiFi LED off
 	}
 
