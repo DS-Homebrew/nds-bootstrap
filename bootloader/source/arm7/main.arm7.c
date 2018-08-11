60553afd6f233f0b284a9c6486c7e3d9e5066865
Damiano Amatruda <damiano.amatruda@outlook.com>
2018-08-12 01:28:04 +0200

    Remove commented out code about fatSize

--- bootloader/source/arm7/main.arm7.c
+++ bootloader/source/arm7/main.arm7.c
@@ -89,7 +89,6 @@ extern unsigned long asyncPrefetch;
 bool dsiModeConfirmed = false; // SDK 5
 
 u32 ROMinRAM = false;
-//u32 fatSize;
 //u32 romSize;
 //u32 romSizeNoArm9;
 
@@ -626,7 +625,6 @@ void loadBinary_ARM7(aFile file) {
 	u32 ARM7_SRC   = dsiHeaderTemp.ndshdr.arm7romOffset;
 	char* ARM7_DST = (char*)dsiHeaderTemp.ndshdr.arm7destination;
 
-	//fatSize       = dsiHeaderTemp.ndshdr.fatSize;
 	//romSize       = dsiHeaderTemp.ndshdr.romSize;
 	//romSizeNoArm9 = dsiHeaderTemp.ndshdr.romSize - 0x4000 - dsiHeaderTemp.ndshdr.arm9binarySize;
 
