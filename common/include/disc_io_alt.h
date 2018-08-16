b0c9c720914b4d1a22975f27533d95333f5eb246
Damiano Amatruda <damiano.amatruda@outlook.com>
2018-08-16 12:09:18 +0200

    Reorganize and rewrite some parts of I/O code

--- common/include/disc_io_alt.h
+++ common/include/disc_io_alt.h
@@ -1,12 +1,11 @@
 /*
  disc_io.h
  Interface template for low level disc functions.
-
  Copyright (c) 2006 Michael "Chishm" Chisholm
-
+ Based on code originally written by MightyMax
+	
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
-
   1. Redistributions of source code must retain the above copyright notice,
      this list of conditions and the following disclaimer.
   2. Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +13,6 @@
      other materials provided with the distribution.
   3. The name of the author may not be used to endorse or promote products derived
      from this software without specific prior written permission.
-
  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
  AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
@@ -24,13 +22,6 @@
  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-	2006-07-11 - Chishm
-		* Original release
-
-	2006-07-16 - Chishm
-		* Renamed _CF_USE_DMA to _IO_USE_DMA
-		* Renamed _CF_ALLOW_UNALIGNED to _IO_ALLOW_UNALIGNED
 */
 
 #ifndef DISC_IO_H
@@ -39,46 +30,33 @@
 #include <nds/ndstypes.h>
 #define BYTES_PER_SECTOR 512
 
-//----------------------------------------------------------------------
-// Customisable features
-
-// Use DMA to read the card, remove this line to use normal reads/writes
-// #define _IO_USE_DMA
-
-// Allow buffers not alligned to 16 bits when reading files. 
-// Note that this will slow down access speed, so only use if you have to.
-// It is also incompatible with DMA
-#define _IO_ALLOW_UNALIGNED
-
-#if defined _IO_USE_DMA && defined _IO_ALLOW_UNALIGNED
- #error "You can't use both DMA and unaligned memory"
-#endif
-
 #define FEATURE_MEDIUM_CANREAD		0x00000001
 #define FEATURE_MEDIUM_CANWRITE		0x00000002
 #define FEATURE_SLOT_GBA			0x00000010
 #define FEATURE_SLOT_NDS			0x00000020
 
-typedef bool (* FN_MEDIUM_STARTUP)(void);
-typedef bool (* FN_MEDIUM_ISINSERTED)(void);
-typedef bool (* FN_MEDIUM_READSECTORS)(u32 sector, u32 numSectors, void* buffer, int ndmaSlot);
-typedef bool (* FN_MEDIUM_WRITESECTORS)(u32 sector, u32 numSectors, const void* buffer, int ndmaSlot);
-typedef bool (* FN_MEDIUM_CLEARSTATUS)(void);
-typedef bool (* FN_MEDIUM_SHUTDOWN)(void);
+#define DEVICE_TYPE_DSI_SD ('i') | ('_' << 8) | ('S' << 16) | ('D' << 24)
 
-struct IO_INTERFACE_STRUCT {
-	unsigned long			ioType;
-	unsigned long			features;
-	FN_MEDIUM_STARTUP		fn_startup;
-	FN_MEDIUM_ISINSERTED	fn_isInserted;
-	FN_MEDIUM_READSECTORS	fn_readSectors;
-	FN_MEDIUM_WRITESECTORS	fn_writeSectors;
-	FN_MEDIUM_CLEARSTATUS	fn_clearStatus;
-	FN_MEDIUM_SHUTDOWN		fn_shutdown;
-};
+typedef bool (* FN_MEDIUM_STARTUP)(void) ;
+typedef bool (* FN_MEDIUM_ISINSERTED)(void) ;
+typedef bool (* FN_MEDIUM_READSECTORS)(sec_t sector, sec_t numSectors, void* buffer, int ndmaSlot) ;
+typedef bool (* FN_MEDIUM_WRITESECTORS)(sec_t sector, sec_t numSectors, const void* buffer, int ndmaSlot) ;
+typedef bool (* FN_MEDIUM_CLEARSTATUS)(void) ;
+typedef bool (* FN_MEDIUM_SHUTDOWN)(void) ;
 
-typedef struct IO_INTERFACE_STRUCT IO_INTERFACE;
+struct DISC_INTERFACE_STRUCT {
+	unsigned long			ioType ;
+	unsigned long			features ;
+	FN_MEDIUM_STARTUP		startup ;
+	FN_MEDIUM_ISINSERTED	isInserted ;
+	FN_MEDIUM_READSECTORS	readSectors ;
+	FN_MEDIUM_WRITESECTORS	writeSectors ;
+	FN_MEDIUM_CLEARSTATUS	clearStatus ;
+	FN_MEDIUM_SHUTDOWN		shutdown ;
+} ;
 
-extern IO_INTERFACE __myio_dsisd;
+typedef struct DISC_INTERFACE_STRUCT DISC_INTERFACE ;
 
-#endif // DISC_IO_H
+const DISC_INTERFACE* get_io_dsisd (void);
+
+#endif	// define NDS_DISC_IO_INCLUDE
