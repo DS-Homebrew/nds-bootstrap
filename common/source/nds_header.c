24aba36c9abae2b0be505477bf4449dd8c50ff47
Damiano Amatruda <damiano.amatruda@outlook.com>
2018-08-17 01:50:12 +0200

    Fix indentation

--- common/source/nds_header.c
+++ common/source/nds_header.c
@@ -1,9 +1,9 @@
 #include "nds_header.h"
 
 const char* getRomTid(const tNDSHeader* ndsHeader) {
-    //u32 ROM_TID = *(u32*)ndsHeader->gameCode;
+	//u32 ROM_TID = *(u32*)ndsHeader->gameCode;
 	static char romTid[5];
-    strncpy(romTid, ndsHeader->gameCode, 4);
-    romTid[4] = '\0';
-    return romTid;
+	strncpy(romTid, ndsHeader->gameCode, 4);
+	romTid[4] = '\0';
+	return romTid;
 }
