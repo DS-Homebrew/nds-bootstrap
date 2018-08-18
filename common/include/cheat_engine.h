9e691046b766c651e435ddda76a3fbca14843d2d
Damiano Amatruda <damiano.amatruda@outlook.com>
2018-08-18 14:41:38 +0200

    Create “checkCheatDataLen” function in “cheat_engine.h”

--- common/include/cheat_engine.h
+++ common/include/cheat_engine.h
@@ -13,6 +13,10 @@
     *cheat_data_len_ptr += 2;
 }*/
 
+inline bool checkCheatDataLen(u32 cheat_data_len) {
+    return (cheat_data_len + 2 <= CHEAT_DATA_MAX_LEN);
+}
+
 inline void endCheatData(u32* cheat_data, u32 cheat_data_len) {
     if (cheat_data_len + 2 > CHEAT_DATA_MAX_LEN) { // Not necessarily needed
         return;
