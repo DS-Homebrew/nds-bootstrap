584cd54a54a8a4b6bd7340b827683c955a1ae271
RocketRobz <bobesh8@gmail.com>
2019-01-30 15:13:52 -0700

    Custom loading screen: Make loading bar red instead of showing text when error occurs

--- retail/bootloader/source/arm9/loading_screens/loading_screen_r4_like.arm9.c
+++ retail/bootloader/source/arm9/loading_screens/loading_screen_r4_like.arm9.c
@@ -181,151 +181,15 @@ void arm9_loadingCircle2(void) {
 }
 
 void arm9_errorText3(void) {
-	// Cover "Loading..." text
-	for (int i = 0; i < 256*48; i++) {
-		VRAM_A[i] = 0x7FFF;
-	}
-
-	// Draw "Error!" text
-	// E: Part 1
-	for (int y = 12; y <= 44; y++) {
-		for (int k = 76; k <= 79; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// E: Part 2
-	for (int y = 12; y <= 15; y++) {
-		for (int k = 80; k <= 90; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// E: Part 3
-	for (int y = 26; y <= 29; y++) {
-		for (int k = 80; k <= 90; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// E: Part 4
-	for (int y = 41; y <= 44; y++) {
-		for (int k = 80; k <= 90; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// Draw 2 r's.
-	for (int i = 0; i < 2; i++) {
-		// Draw "Error!" text
-		// r: Part 1
-		for (int y = 23; y <= 44; y++) {
-			for (int k = 95; k <= 98; k++) {
-				VRAM_A[y*256+k+i*19] = baseColor;
-			}
-		}
-
-		// Draw "Error!" text
-		// r: Part 2
-		for (int y = 26; y <= 29; y++) {
-			for (int k = 99; k <= 102; k++) {
-				VRAM_A[y*256+k+i*19] = baseColor;
-			}
-		}
-
-		// Draw "Error!" text
-		// r: Part 3
-		for (int y = 23; y <= 26; y++) {
-			for (int k = 103; k <= 109; k++) {
-				VRAM_A[y*256+k+i*19] = baseColor;
-			}
-		}
-	}
-
-	// Draw "Error!" text
-	// o: Part 1
-	for (int y = 23; y <= 26; y++) {
-		for (int k = 137; k <= 147; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// o: Part 2
-	for (int y = 41; y <= 44; y++) {
-		for (int k = 137; k <= 147; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// o: Part 3
-	for (int y = 27; y <= 40; y++) {
-		for (int k = 133; k <= 136; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// o: Part 4
-	for (int y = 27; y <= 40; y++) {
-		for (int k = 148; k <= 151; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// 3rd r: Part 1
-	for (int y = 23; y <= 44; y++) {
-		for (int k = 156; k <= 159; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// 3rd r: Part 2
-	for (int y = 26; y <= 29; y++) {
-		for (int k = 160; k <= 163; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// 3rd r: Part 3
-	for (int y = 23; y <= 26; y++) {
-		for (int k = 164; k <= 170; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// !: Part 1
-	for (int y = 12; y <= 32; y++) {
-		for (int k = 175; k <= 178; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// Draw "Error!" text
-	// !: Part 2
-	for (int y = 38; y <= 44; y++) {
-		for (int k = 175; k <= 178; k++) {
-			VRAM_A[y*256+k] = baseColor;
-		}
-	}
-
-	// End of Draw "Error!" text
-
 	// Change dots of loading circle to form an X
 	drawRectangle (1, 89, 4, 4, errorColor);
 	drawRectangle (11, 89, 4, 4, errorColor);
 	drawRectangle (6, 94, 4, 4, errorColor);
 	drawRectangle (1, 99, 4, 4, errorColor);
 	drawRectangle (11, 99, 4, 4, errorColor);
+
+	// Make loading bar red
+	drawRectangleGradient(16, arm9_loadingBarYpos+2, 28*(arm9_loadBarLength+1), 10, 0, 16, 0);
 
 	arm9_animateLoadingCircle = false;
 	displayScreen = false;
