9a0730df6c8d40cf04c6573d3b01a20743d2e8a3
RocketRobz <bobesh8@gmail.com>
2019-02-26 18:29:44 -0700

    Improve cardId implementation

--- retail/cardengine/arm7/source/cardengine.c
+++ retail/cardengine/arm7/source/cardengine.c
@@ -993,14 +993,57 @@ u32 cardId(void) {
     
     //Devicecapacity         (Chipsize = 128KB SHL nn) (eg. 7 = 16MB)
     //Chip size (00h..7Fh: (N+1)Mbytes, F0h..FFh: (100h-N)*256Mbytes?) (eg. 0Fh = 16MB)
-    u8 mb = (0x20000 << size / 0x10000) - 1; //128KB
+	u8 mb = 0;
+    switch (size) {
+		default:
+			break;
+		case 0x04:
+			mb = 0x01;
+			break;
+		case 0x05:
+			mb = 0x03;
+			break;
+		case 0x06:
+			mb = 0x07;
+			break;
+		case 0x07:
+			mb = 0x0F;
+			break;
+		case 0x08:
+			mb = 0x1F;
+			break;
+		case 0x09:
+			mb = 0x3F;
+			break;
+		case 0x0A:
+			mb = 0x7F;
+			break;
+		case 0x0B:
+			mb = 0xFF;
+			break;
+		case 0x0C:
+			mb = 0xFE;
+			break;
+	}
     cardid |= mb << 16;
-    	
+
+	//The Flag Bits in 3th byte can be
+	//0   Maybe Infrared flag? (in case ROM does contain on-chip infrared stuff)
+	if (strncmp(getRomTid(ndsHeader), "I", 1) == 0) {
+		cardid |= 0x01 << 8;
+	}
+
     //The Flag Bits in 4th byte can be
     //6   DSi flag (0=NDS/3DS, 1=DSi)
     //7   Cart Protocol Variant (0=older/smaller carts, 1=newer/bigger carts)
     u8 unit = 0;
-    if(ndsHeader->unitCode==0x02) unit=0xC0;
+    if (ndsHeader->unitCode==0x02) {
+		unit=0xC0;
+	} else if (strncmp(getRomTid(ndsHeader), "I", 1) == 0) {
+		unit=0xE0;
+	} else if (moduleParams->sdk_version > 0x3000000) {
+		unit=0x80;
+	}
     cardid |= unit;
     
     // Keep the default CardID fow now
