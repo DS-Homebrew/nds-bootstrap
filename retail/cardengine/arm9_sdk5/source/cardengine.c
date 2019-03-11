2d2bcb478bbc5c976192e7fcd52171139c07ccd7
RocketRobz <bobesh8@gmail.com>
2019-03-11 17:11:33 -0600

    Check for isDma insead of sleepRef
    isDma is already true if sleepRef is found
    
    Fixes black screen crash with Ultimate Mortal Kombat using DMA/sleep

--- retail/cardengine/arm9_sdk5/source/cardengine.c
+++ retail/cardengine/arm9_sdk5/source/cardengine.c
@@ -120,7 +120,7 @@ static void sleep(u32 ms) {
 static void waitForArm7(void) {
     IPC_SendSync(0xEE24);
     int count = 0;
-    if (ce9->patches->sleepRef || ce9->thumbPatches->sleepRef) {
+    if (isDma) {
         while (sharedAddr[3] != (vu32)0) {
            if(count==0) {
                 sleep(1);
