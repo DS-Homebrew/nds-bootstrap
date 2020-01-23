99c41aa273fab32679c83c705faaa887bd23a749
RocketRobz <bobesh8@gmail.com>
2020-01-23 12:11:38 -0700

    Activate NDMA card read for Suikoden: Tierkreis

--- retail/bootloader/source/arm7/patch_arm9.c
+++ retail/bootloader/source/arm7/patch_arm9.c
@@ -300,6 +300,7 @@ static void patchCardEndReadDma(cardengineArm9* ce9, const tNDSHeader* ndsHeader
         ||  strncmp(romTid, "CSN", 3) == 0  // Sonic Chronicles: The Dark BrotherHood
         ||  strncmp(romTid, "YT7", 3) == 0  // SEGA Superstars Tennis
         ||  strncmp(romTid, "CB6", 3) == 0  // Space Bust-A-Move // works, fixes lags
+        ||  strncmp(romTid, "YG4", 3) == 0  // Suikoden: Tierkreis // works
         ||  strncmp(romTid, "YUT", 3) == 0  // Ultimate Mortal Kombat
         ||  strncmp(romTid, "A8Q", 3) == 0  // Theme Park // works
         ||  strncmp(romTid, "AH9", 3) == 0  // Tony Hawk's American Sk8land // works, fixes crashing
