6a255fc38e0a653a24520f5a57914c4d1458e5e9
RocketRobz <bobesh8@gmail.com>
2020-11-10 18:31:49 -0700

    Improve Slot-1 check for patching sleep mode

--- retail/bootloader/source/arm7/patch_arm7.c
+++ retail/bootloader/source/arm7/patch_arm7.c
@@ -103,7 +103,8 @@ static void patchSleepMode(const tNDSHeader* ndsHeader) {
 		}
 		patchOffsetCache.sleepPatchOffset = sleepPatchOffset;
 	}
-	if (REG_SCFG_EXT == 0 || forceSleepPatch || REG_SCFG_MC == 0x11) {
+	if (REG_SCFG_EXT == 0 || (REG_SCFG_MC & BIT(0)) || (!(REG_SCFG_MC & BIT(2)) && !(REG_SCFG_MC & BIT(3)))
+	 || forceSleepPatch) {
 		if (sleepPatchOffset) {
 			// Patch
 			*((u16*)sleepPatchOffset + 2) = 0;
