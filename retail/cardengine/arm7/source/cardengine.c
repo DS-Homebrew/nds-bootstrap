fb556eb9abf0d8ceaa23bbea3ea3cb558ca3372d
RocketRobz <bobesh8@gmail.com>
2020-11-11 21:53:18 -0700

    ce7: Disable I2C writes during save writing
    Prevents crashes and save corruption in some games

--- retail/cardengine/arm7/source/cardengine.c
+++ retail/cardengine/arm7/source/cardengine.c
@@ -926,7 +926,7 @@ void myIrqHandlerVBlank(void) {
 	if (saveTimer > 0) {
 		saveTimer++;
 		if (saveTimer == 60) {
-			i2cWriteRegister(0x4A, 0x12, 0x00);		// If saved, power button works again.
+			//i2cWriteRegister(0x4A, 0x12, 0x00);		// If saved, power button works again.
 			saveTimer = 0;
 		}
 	}
@@ -1070,7 +1070,7 @@ bool eepromPageWrite(u32 dst, const void *src, u32 len) {
 		driveInitialize();
 		sdRead = ((valueBits & saveOnFlashcard) ? false : true);
 		saveTimer = 1;
-		i2cWriteRegister(0x4A, 0x12, 0x01);		// When we're saving, power button does nothing, in order to prevent corruption.
+		//i2cWriteRegister(0x4A, 0x12, 0x01);		// When we're saving, power button does nothing, in order to prevent corruption.
 		if (saveInRam) {
 			tonccpy((char*)0x02440000 + dst, src, len);
 		}
@@ -1101,7 +1101,7 @@ bool eepromPageProg(u32 dst, const void *src, u32 len) {
 		driveInitialize();
 		sdRead = ((valueBits & saveOnFlashcard) ? false : true);
 		saveTimer = 1;
-		i2cWriteRegister(0x4A, 0x12, 0x01);		// When we're saving, power button does nothing, in order to prevent corruption.
+		//i2cWriteRegister(0x4A, 0x12, 0x01);		// When we're saving, power button does nothing, in order to prevent corruption.
 		if (saveInRam) {
 			tonccpy((char*)0x02440000 + dst, src, len);
 		}
