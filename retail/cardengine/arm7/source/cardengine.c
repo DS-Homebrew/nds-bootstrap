e407960284dd3661d79f539b01f79a14d6792d5b
JDSft <cuentadtt@gmail.com>
2021-01-31 08:46:08 +0100

    Testing IPC 4

--- retail/cardengine/arm7/source/cardengine.c
+++ retail/cardengine/arm7/source/cardengine.c
@@ -790,8 +790,9 @@ void myIrqHandlerVBlank(void) {
 			if (swapTimer == 60){
 				int oldIME = enterCriticalSection();
 				swapTimer = 0;
-				sharedAddr[4] = 0x50000000;
+				sharedAddr[4] = 0x57534352;
 				IPC_SendSync(0x8);
+				waitFrames(5);
 				leaveCriticalSection(oldIME);
 			}
 		}
--- retail/cardengine/arm9/source/cardengine.c
+++ retail/cardengine/arm9/source/cardengine.c
@@ -1060,7 +1060,7 @@ void myIrqHandlerIPC(void) {
 	}
 #endif
 
-	if (sharedAddr[4] == (vu32)0x50000000) {
+	if (sharedAddr[4] == (vu32)0x57534352) {
 		int oldIME = enterCriticalSection();
 		lcdSwap();
 		leaveCriticalSection(oldIME);
