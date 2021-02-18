1050cfe9859de58ad2a5825d68d0951d211d4699
RocketRobz <bobesh8@gmail.com>
2021-02-18 11:16:55 -0700

    In-game menu: Backup original tile and palette, and restore, when exiting

--- retail/bootloader/source/arm7/main.arm7.c
+++ retail/bootloader/source/arm7/main.arm7.c
@@ -242,7 +242,7 @@ static void resetMemory_ARM7(void) {
 	toncset((u32*)0x02004000, 0, 0x33C000);	// clear part of EWRAM - except before nds-bootstrap images
 	toncset((u32*)0x02380000, 0, 0x5A000);		// clear part of EWRAM - except before 0x023DA000, which has the arm9 code
 	toncset((u32*)0x023DB000, 0, 0x25000);		// clear part of EWRAM
-	toncset((u32*)0x02400000, 0, 0x3BAE00);	// clear part of EWRAM - except before ce7 and ce9 binaries
+	toncset((u32*)0x02400000, 0, 0x3B9C00);	// clear part of EWRAM - except before ce7 and ce9 binaries
 	toncset((u32*)0x027F8000, 0, 0x8000);		// clear part of EWRAM
 	toncset((u32*)0x02D00000, 0, 0x2FE000);	// clear part of EWRAM
 	toncset((u32*)0x02FFF000, 0, 0x1000);		// clear part of EWRAM: header
--- retail/cardengine/arm9/source/inGameMenu.c
+++ retail/cardengine/arm9/source/inGameMenu.c
@@ -14,17 +14,13 @@ extern vu32* volatile sharedAddr;
 void inGameMenu(void) {
 	int oldIME = enterCriticalSection();
 
-	// The commented code doesn't seem to work
-	/*u32 dispCntBak = REG_DISPCNT;
-	REG_DISPCNT = 0x20000;
-	*(vu8*)0x4000240 = 0x80;
-	*(vu16*)0x6800000 = 0x8000;*/
-
 	u32 dspcnt = REG_DISPCNT;
 	u16 bg0cnt = REG_BG0CNT;
 
 	REG_DISPCNT = 0x10100;
 	REG_BG0CNT = 1 << 8;
+
+	tonccpy((u16*)0x0277FF00, BG_PALETTE, 256*sizeof(u16));	// Backup the palette
 
 	*(u32*)BG_PALETTE = 0xFFFF0000; // First palette black, second white
 	BG_MAP_RAM(1)[0] = 1;
@@ -38,6 +34,7 @@ void inGameMenu(void) {
 		0x10,0x22,0x22,0x01,
 		0x00,0x11,0x11,0x00,
 	};
+	tonccpy((u16*)0x02770000, BG_GFX + (sizeof(smile) / 2), sizeof(smile));	// Backup the original tile
 	tonccpy(BG_GFX + (sizeof(smile) / 2), smile, sizeof(smile));
 
 	u8 prevPosition = 0;
@@ -50,10 +47,11 @@ void inGameMenu(void) {
 		}
 	}
 
+	tonccpy(BG_PALETTE, (u16*)0x0277FF00, 256*sizeof(u16));	// Restore the palette
+	tonccpy(BG_GFX + (sizeof(smile) / 2), (u16*)0x02770000, sizeof(smile));	// Restore the original tile
+
 	REG_DISPCNT = dspcnt;
 	REG_BG0CNT = bg0cnt;
-
-	//REG_DISPCNT = dispCntBak;
 
 	leaveCriticalSection(oldIME);
 }
