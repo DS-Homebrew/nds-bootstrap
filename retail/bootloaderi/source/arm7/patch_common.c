ede60b2ffb6707572a73e048235c357852260c68
RocketRobz <bobesh8@gmail.com>
2021-05-04 19:41:07 -0600

    Fix Pokemon HGSS freezing issue

--- retail/bootloaderi/source/arm7/patch_common.c
+++ retail/bootloaderi/source/arm7/patch_common.c
@@ -380,6 +380,15 @@ void patchBinary(const tNDSHeader* ndsHeader) {
         *(u32*)0x02000C14 = 0xE1A00000; //nop
 	}
 
+    // Pokemon HeartGold & SoulSilver
+	if (strcmp(romTid, "IPKJ") == 0 || strcmp(romTid, "IPGJ") == 0) {
+        *(u32*)0x20DD9E4 = 0xE1A00000; //nop
+	} else if (strcmp(romTid, "IPKK") == 0 || strcmp(romTid, "IPGK") == 0) {
+        *(u32*)0x20DE860 = 0xE1A00000; //nop
+	} else if (strncmp(romTid, "IPK", 3) == 0 || strncmp(romTid, "IPG", 3) == 0) {
+        *(u32*)0x20DE16C = 0xE1A00000; //nop
+	}
+
     // Golden Sun
     if (strcmp(romTid, "BO5E") == 0) {
         // patch "refresh" function
