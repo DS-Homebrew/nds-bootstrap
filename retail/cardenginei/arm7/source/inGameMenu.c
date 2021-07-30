5cc5756fb19982fff2e2656d60949f7beb945878
Pk11 <epicpkmn11@outlook.com>
2021-07-30 07:16:03 -0500

    Add stepping 1 frame with R in menu
    Also: Clean up whitespace in ARM7 and add RTL handling for screenshot submenu

--- retail/cardenginei/arm7/source/inGameMenu.c
+++ retail/cardenginei/arm7/source/inGameMenu.c
@@ -9,9 +9,10 @@
 #include "cardengine.h"
 #include "nds_header.h"
 
-#define	REG_EXTKEYINPUT	(*(vuint16*)0x04000136)
+#define REG_EXTKEYINPUT (*(vuint16*)0x04000136)
 
 extern vu32* volatile sharedAddr;
+extern bool returnToMenu;
 
 extern void forceGameReboot(void);
 extern void dumpRam(void);
@@ -21,6 +22,7 @@ extern void saveScreenshot(void);
 volatile int timeTilBatteryLevelRefresh = 7;
 
 void inGameMenu(void) {
+	returnToMenu = false;
 	sharedAddr[4] = 0x554E454D; // 'MENU'
 	IPC_SendSync(0x9);
 	REG_MASTER_VOLUME = 0;
@@ -39,40 +41,43 @@ void inGameMenu(void) {
 	}
 
 	if (sharedAddr[4] == 0x554E454D) {
-	  while (1) {
-		sharedAddr[5] = ~REG_KEYINPUT & 0x3FF;
-		sharedAddr[5] |= ((~REG_EXTKEYINPUT & 0x3) << 10) | ((~REG_EXTKEYINPUT & 0xC0) << 6);
-		timeTilBatteryLevelRefresh++;
-		if (timeTilBatteryLevelRefresh == 8) {
-			*(u8*)(INGAME_MENU_LOCATION+0x9FFF) = i2cReadRegister(I2C_PM, I2CREGPM_BATTERY);
-			timeTilBatteryLevelRefresh = 0;
-		}
+		while (1) {
+			sharedAddr[5] = ~REG_KEYINPUT & 0x3FF;
+			sharedAddr[5] |= ((~REG_EXTKEYINPUT & 0x3) << 10) | ((~REG_EXTKEYINPUT & 0xC0) << 6);
+			timeTilBatteryLevelRefresh++;
+			if (timeTilBatteryLevelRefresh == 8) {
+				*(u8*)(INGAME_MENU_LOCATION+0x9FFF) = i2cReadRegister(I2C_PM, I2CREGPM_BATTERY);
+				timeTilBatteryLevelRefresh = 0;
+			}
 
-		while (REG_VCOUNT != 191);
-		while (REG_VCOUNT == 191);
+			while (REG_VCOUNT != 191);
+			while (REG_VCOUNT == 191);
 
-		switch (sharedAddr[4]) {
-			case 0x54495845: // EXIT
-			default:
+			switch (sharedAddr[4]) {
+				case 0x54495845: // EXIT
+				default:
+					break;
+				case 0x54455352: // RSET
+					forceGameReboot();
+					break;
+				case 0x54495551: // QUIT
+					returnToLoader();
+					break;
+				case 0x444D4152: // RAMD
+					dumpRam();
+					break;
+				case 0x544F4853: // SHOT
+					saveScreenshot();
+					sharedAddr[4] = 0x554E454D;
+					break;
+				case 0x50455453: // STEP
+					returnToMenu = true;
+					break;
+			}
+			if (sharedAddr[4] != 0x554E454D) {
 				break;
-			case 0x54455352: // RSET
-				forceGameReboot();
-				break;
-			case 0x54495551: // QUIT
-				returnToLoader();
-				break;
-			case 0x444D4152: // RAMD
-				dumpRam();
-				break;
-			case 0x544F4853: // SHOT
-				saveScreenshot();
-				sharedAddr[4] = 0x554E454D;
-				break;
+			}
 		}
-		if (sharedAddr[4] != 0x554E454D) {
-			break;
-		}
-	  }
 	}
 
 	sharedAddr[4] = 0x54495845; // EXIT
