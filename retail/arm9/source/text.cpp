ff5d7ada00f93a3b7ef541a6afba0bfd650024ba
Pk11 <epicpkmn11@outlook.com>
2021-09-05 22:30:50 -0500

    Improve Arabic font and add لا ligature
    Thanks to heydootdoot for improving the font!
    
    Co-authored by @heydootdoot

--- retail/arm9/source/text.cpp
+++ retail/arm9/source/text.cpp
@@ -1,6 +1,9 @@
 #include "text.h"
 #include "tonccpy.h"
 
+#include <array>
+#include <map>
+
 constexpr char16_t map[] =
 	u"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"// Special
 	u"¡«º»¿           " // Extended punctuation, before ASCII to keep it normal
@@ -35,7 +38,7 @@ constexpr char16_t map[] =
 	u"ﺧﺨﺪﺬﺮﺰﺲﺳﺴﺶﺷﺸﺺﺻﺼﺾ"
 	u"ﺿﻀﻂﻃﻄﻆﻇﻈﻊﻋﻌﻎﻏﻐﻒﻓ"
 	u"ﻔﻖﻗﻘﻚﻛﻜﻞﻟﻠﻢﻣﻤﻦﻧﻨ"
-	u"ﻪﻫﻬﻮﻰﻲﻳﻴ،؟      "
+	u"ﻪﻫﻬﻮﻰﻲﻳﻴ،؟ﻻﻼ    "
 	u"　｡｢｣､･ｦｧｨｩｪｫｬｭｮｯ" // Kana
 	u"ｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿ"
 	u"ﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏ"
@@ -49,53 +52,55 @@ constexpr char16_t map[] =
 	u"뱅뷰상샷선설셋소속스아어운위으이"
 	u"인임자정종주카켬크클택트프향화";
 
-constexpr char16_t arabicPresentationForms[][3] = {
+std::map<char16_t, std::array<char16_t, 3>> arabicPresentationForms = {
 	// Initial, Medial, Final
-	{u'آ', u'ﺂ', u'ﺂ'}, // Alef with madda above
-	{u'أ', u'ﺄ', u'ﺄ'}, // Alef with hamza above
-	{u'ؤ', u'ﺆ', u'ﺆ'}, // Waw with hamza above
-	{u'إ', u'ﺈ', u'ﺈ'}, // Alef with hamza below
-	{u'ﺋ', u'ﺌ', u'ﺊ'}, // Yeh with hamza above
-	{u'ا', u'ﺎ', u'ﺎ'}, // Alef
-	{u'ﺑ', u'ﺒ', u'ﺐ'}, // Beh
-	{u'ة', u'ﺔ', u'ﺔ'}, // Teh marbuta
-	{u'ﺗ', u'ﺘ', u'ﺖ'}, // Teh
-	{u'ﺛ', u'ﺜ', u'ﺚ'}, // Theh
-	{u'ﺟ', u'ﺠ', u'ﺞ'}, // Jeem
-	{u'ﺣ', u'ﺤ', u'ﺢ'}, // Hah
-	{u'ﺧ', u'ﺨ', u'ﺦ'}, // Khah
-	{u'د', u'ﺪ', u'ﺪ'}, // Dal
-	{u'ذ', u'ﺬ', u'ﺬ'}, // Thal
-	{u'ر', u'ﺮ', u'ﺮ'}, // Reh
-	{u'ز', u'ﺰ', u'ﺰ'}, // Zain
-	{u'ﺳ', u'ﺴ', u'ﺲ'}, // Seen
-	{u'ﺷ', u'ﺸ', u'ﺶ'}, // Sheen
-	{u'ﺻ', u'ﺼ', u'ﺺ'}, // Sad
-	{u'ﺿ', u'ﻀ', u'ﺾ'}, // Dad
-	{u'ﻃ', u'ﻄ', u'ﻂ'}, // Tah
-	{u'ﻇ', u'ﻈ', u'ﻆ'}, // Zah
-	{u'ﻋ', u'ﻌ', u'ﻊ'}, // Ain
-	{u'ﻏ', u'ﻐ', u'ﻎ'}, // Ghain
-	{u'ػ', u'ػ', u'ػ'}, // Keheh with two dots above
-	{u'ؼ', u'ؼ', u'ؼ'}, // Keheh with three dots below
-	{u'ؽ', u'ؽ', u'ؽ'}, // Farsi yeh with inverted v
-	{u'ؾ', u'ؾ', u'ؾ'}, // Farsi yeh with two dots above
-	{u'ؿ', u'ؿ', u'ؿ'}, // Farsi yeh with three docs above
-	{u'ـ', u'ـ', u'ـ'}, // Tatweel
-	{u'ﻓ', u'ﻔ', u'ﻒ'}, // Feh
-	{u'ﻗ', u'ﻘ', u'ﻖ'}, // Qaf
-	{u'ﻛ', u'ﻜ', u'ﻚ'}, // Kaf
-	{u'ﻟ', u'ﻠ', u'ﻞ'}, // Lam
-	{u'ﻣ', u'ﻤ', u'ﻢ'}, // Meem
-	{u'ﻧ', u'ﻨ', u'ﻦ'}, // Noon
-	{u'ﻫ', u'ﻬ', u'ﻪ'}, // Heh
-	{u'و', u'ﻮ', u'ﻮ'}, // Waw
-	{u'ﯨ', u'ﯩ', u'ﻰ'}, // Alef maksura
-	{u'ﻳ', u'ﻴ', u'ﻲ'}, // Yeh
+	{u'آ', {u'آ', u'ﺂ', u'ﺂ'}}, // Alef with madda above
+	{u'أ', {u'أ', u'ﺄ', u'ﺄ'}}, // Alef with hamza above
+	{u'ؤ', {u'ؤ', u'ﺆ', u'ﺆ'}}, // Waw with hamza above
+	{u'إ', {u'إ', u'ﺈ', u'ﺈ'}}, // Alef with hamza below
+	{u'ئ', {u'ﺋ', u'ﺌ', u'ﺊ'}}, // Yeh with hamza above
+	{u'ا', {u'ا', u'ﺎ', u'ﺎ'}}, // Alef
+	{u'ب', {u'ﺑ', u'ﺒ', u'ﺐ'}}, // Beh
+	{u'ة', {u'ة', u'ﺔ', u'ﺔ'}}, // Teh marbuta
+	{u'ت', {u'ﺗ', u'ﺘ', u'ﺖ'}}, // Teh
+	{u'ث', {u'ﺛ', u'ﺜ', u'ﺚ'}}, // Theh
+	{u'ج', {u'ﺟ', u'ﺠ', u'ﺞ'}}, // Jeem
+	{u'ح', {u'ﺣ', u'ﺤ', u'ﺢ'}}, // Hah
+	{u'خ', {u'ﺧ', u'ﺨ', u'ﺦ'}}, // Khah
+	{u'د', {u'د', u'ﺪ', u'ﺪ'}}, // Dal
+	{u'ذ', {u'ذ', u'ﺬ', u'ﺬ'}}, // Thal
+	{u'ر', {u'ر', u'ﺮ', u'ﺮ'}}, // Reh
+	{u'ز', {u'ز', u'ﺰ', u'ﺰ'}}, // Zain
+	{u'س', {u'ﺳ', u'ﺴ', u'ﺲ'}}, // Seen
+	{u'ش', {u'ﺷ', u'ﺸ', u'ﺶ'}}, // Sheen
+	{u'ص', {u'ﺻ', u'ﺼ', u'ﺺ'}}, // Sad
+	{u'ض', {u'ﺿ', u'ﻀ', u'ﺾ'}}, // Dad
+	{u'ط', {u'ﻃ', u'ﻄ', u'ﻂ'}}, // Tah
+	{u'ظ', {u'ﻇ', u'ﻈ', u'ﻆ'}}, // Zah
+	{u'ع', {u'ﻋ', u'ﻌ', u'ﻊ'}}, // Ain
+	{u'غ', {u'ﻏ', u'ﻐ', u'ﻎ'}}, // Ghain
+	{u'ػ', {u'ػ', u'ػ', u'ػ'}}, // Keheh with two dots above
+	{u'ؼ', {u'ؼ', u'ؼ', u'ؼ'}}, // Keheh with three dots below
+	{u'ؽ', {u'ؽ', u'ؽ', u'ؽ'}}, // Farsi yeh with inverted v
+	{u'ؾ', {u'ؾ', u'ؾ', u'ؾ'}}, // Farsi yeh with two dots above
+	{u'ؿ', {u'ؿ', u'ؿ', u'ؿ'}}, // Farsi yeh with three docs above
+	{u'ـ', {u'ـ', u'ـ', u'ـ'}}, // Tatweel
+	{u'ف', {u'ﻓ', u'ﻔ', u'ﻒ'}}, // Feh
+	{u'ق', {u'ﻗ', u'ﻘ', u'ﻖ'}}, // Qaf
+	{u'ك', {u'ﻛ', u'ﻜ', u'ﻚ'}}, // Kaf
+	{u'ل', {u'ﻟ', u'ﻠ', u'ﻞ'}}, // Lam
+	{u'م', {u'ﻣ', u'ﻤ', u'ﻢ'}}, // Meem
+	{u'ن', {u'ﻧ', u'ﻨ', u'ﻦ'}}, // Noon
+	{u'ه', {u'ﻫ', u'ﻬ', u'ﻪ'}}, // Heh
+	{u'و', {u'و', u'ﻮ', u'ﻮ'}}, // Waw
+	{u'ى', {u'ﯨ', u'ﯩ', u'ﻰ'}}, // Alef maksura
+	{u'ي', {u'ﻳ', u'ﻴ', u'ﻲ'}}, // Yeh
+
+	{u'ﻻ', {u'ﻻ', u'ﻼ', u'ﻼ'}}, // Ligature lam with alef
 };
 
 // Can't do a binary search as the map isn't fully sorted (so the image looks better)
-u16 getIndex(char16_t c) {
+constexpr u16 getIndex(char16_t c) {
 	for(uint i = 0; i < sizeof(map) / sizeof(map[0]); i++) {
 		if(c == map[i]) {
 			return i;
@@ -107,12 +112,12 @@ u16 getIndex(char16_t c) {
 
 // Specifically the Arabic letters that have supported presentation forms
 bool isArabic(char16_t c) {
-	return c >= 0x0622 && c <= 0x064A;
+	return (c >= 0x0622 && c <= 0x064A) || c == 0xFEFB;
 }
 
 bool isStrongRTL(char16_t c) {
 	// Hebrew, Arabic, or RLM
-	return (c >= 0x0590 && c <= 0x05FF) || (c >= 0x0600 && c <= 0x06FF) || c == 0x200F;
+	return (c >= 0x0590 && c <= 0x05FF) || (c >= 0x0600 && c <= 0x06FF) || (c >= 0xFE70 && c <= 0xFEFC) || c == 0x200F;
 }
 
 bool isWeak(char16_t c) {
@@ -128,12 +133,12 @@ char16_t arabicForm(char16_t current, char16_t prev, char16_t next) {
 		// If previous should be connected to
 		if((prev >= 0x626 && prev <= 0x62E && prev != 0x627 && prev != 0x629) || (prev >= 0x633 && prev <= 0x64A && prev != 0x648)) {
 			if(isArabic(next)) // If next is arabic, medial
-				return arabicPresentationForms[current - 0x622][1];
+				return arabicPresentationForms[current][1];
 			else // If not, final
-				return arabicPresentationForms[current - 0x622][2];
+				return arabicPresentationForms[current][2];
 		} else {
 			if(isArabic(next)) // If next is arabic, initial
-				return arabicPresentationForms[current - 0x622][0];
+				return arabicPresentationForms[current][0];
 			else // If not, isolated
 				return current;
 		}
@@ -279,8 +284,12 @@ void setIgmString(const char *src, u16 *dst) {
 			codepoint = '?';
 		}
 
-		// Add to string
-		*(dst++) = getIndex(codepoint);
+		if(codepoint == u'ا' && dst > dstStart && dst[-1] == getIndex(u'ل')) { // لا ligature
+			dst[-1] = getIndex(u'ﻻ');
+		} else { // Add to string
+			*(dst++) = getIndex(codepoint);
+		}
+
 
 		if((codepoint >= 0x0590 && codepoint <= 0x05FF) || (codepoint >= 0x0600 && codepoint <= 0x06FF) || codepoint == 0x200F)
 			containsRTL = true;
