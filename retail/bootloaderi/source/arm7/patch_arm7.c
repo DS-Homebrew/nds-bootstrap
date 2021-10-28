b516c73b97ccb578f4057c1589ea09828e4855b4
RocketRobz <bobesh8@gmail.com>
2021-10-28 14:48:58 -0600

    Derp fix

--- retail/bootloaderi/source/arm7/patch_arm7.c
+++ retail/bootloaderi/source/arm7/patch_arm7.c
@@ -317,7 +317,7 @@ void patchPostBoot(const tNDSHeader* ndsHeader) {
 	const char* romTid = getRomTid(ndsHeader);
 
 	if (REG_SCFG_EXT != 0 || ndsHeader->unitCode == 0 || !dsiModeConfirmed
-	|| ((ndsHeader->unitCode == 2 || strncmp(romTid, "KD9", 3) == 0 || strncmp(romTid, "KP6", 3) == 0 || strncmp(romTid, "KP6", 3) == 0)
+	|| ((ndsHeader->unitCode == 2 || strncmp(romTid, "KD9", 3) == 0 || strncmp(romTid, "KP6", 3) == 0 || strncmp(romTid, "KAM", 3) == 0)
 	&& oldArm7mbk == 0x00403000)
 	|| *(u32*)0x02FFE1A0 != 0x00403000) {
 		return;
