24d945d507a313af64bd77a0aeac5e97f489b67e
RocketRobz <bobesh8@gmail.com>
2022-06-13 22:04:17 -0600

    hb: Bug fix

--- hb/arm9/libeasysave/source/ini/fetch.cpp
+++ hb/arm9/libeasysave/source/ini/fetch.cpp
@@ -38,7 +38,7 @@ std::string ini::fetch(std::string section, std::string key_name,
                        std::string default_value) {
   int section_index = m_match_section_index(section);
   if (section_index < 0)
-    return "";
+    return default_value;
 
   int key_index = m_match_key_index(section_index, key_name);
   if (key_index < 0) {
