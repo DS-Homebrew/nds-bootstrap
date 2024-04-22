71b3f4e19f8db1c56e103a1600ed943b63f4db7a
RocketRobz <bobesh8@gmail.com>
2024-04-21 19:03:19 -0600

    Add more ld/st instructions

--- retail/cardengine/arm9/source/exception.arm.c
+++ retail/cardengine/arm9/source/exception.arm.c
@@ -14,26 +14,28 @@ extern s8 mainScreen;
 extern vu32* volatile sharedAddr;
 
 #ifndef NODSIWARE
-static u32* debugArea = (u32*)0x02004000;
+// static u32* debugArea = (u32*)0x02004000;
 
 //---------------------------------------------------------------------------------
 bool newSlot2Access() {
 //---------------------------------------------------------------------------------
-	static u32 opcode = 0;
+	u32 opcode = 0;
 	opcode = (*(u32*)0x027FFD98)-8;
 	if (opcode < 0x01FF8000 && opcode >= 0x02400000) {
 		return false;
 	}
 	opcode = *(u32*)opcode;
 
-	debugArea[0] = opcode;
-	debugArea[1] = 0;
+	// debugArea[0] = opcode;
+	// debugArea[1] = 0;
 
 	int regL = 0, regR = 0;
+	u32 opcodePushPop = opcode;
 
 	{
 		while (opcode >= 0x10000000) {
 			opcode -= 0x10000000;
+			opcodePushPop -= 0x10000000;
 		}
 
 		u32 opcodeTemp = opcode;
@@ -48,6 +50,7 @@ bool newSlot2Access() {
 		while (opcodeTemp >= 0x00010000) {
 			opcodeTemp -= 0x00010000;
 			opcode -= 0x00010000;
+			opcodePushPop -= 0x00010000;
 			regR++;
 			if (regR > 12) return false;
 		}
@@ -60,8 +63,8 @@ bool newSlot2Access() {
 		}
 	}
 
-	debugArea[0] = opcode;
-	debugArea[1] = 1;
+	/* debugArea[0] = opcode;
+	debugArea[1] = 1; */
 	/* debugArea[1] = regR;
 	debugArea[2] = regL; */
 
@@ -78,15 +81,33 @@ bool newSlot2Access() {
 	u32 opcodeAlt = opcode;
 	opcode -= offsetChange;
 
-	debugArea[0] = opcode;
-	debugArea[1] = 2;
+	/* debugArea[0] = opcode;
+	debugArea[1] = 2; */
 
+	if (opcodePushPop == 0x09900003) { // ldmib [rR], r0, r1
+		exceptionRegisters[0] = *(u32*)(fixedRegR+4);
+		exceptionRegisters[1] = *(u32*)(fixedRegR+8);
+		return true;
+	} else
+	if (opcodePushPop == 0x09800003) { // stmib [rR], r0, r1
+		*(u32*)(fixedRegR+4) = exceptionRegisters[0];
+		*(u32*)(fixedRegR+8) = exceptionRegisters[1];
+		return true;
+	} else
 	if (opcode == 0x05900000) { // ldr rL, [rR]
 		exceptionRegisters[regL] = *(u32*)(fixedRegR+offsetChange);
 		return true;
 	} else
 	if (opcode == 0x05800000) { // str rL, [rR]
 		*(u32*)(fixedRegR+offsetChange) = exceptionRegisters[regL];
+		return true;
+	} else
+	if (opcode == 0x05100000) { // ldr rL, [rR, #-?]
+		exceptionRegisters[regL] = *(u32*)(fixedRegR-offsetChange);
+		return true;
+	} else
+	if (opcode == 0x05000000) { // str rL, [rR, #-?]
+		*(u32*)(fixedRegR-offsetChange) = exceptionRegisters[regL];
 		return true;
 	} else
 	if (opcode == 0x04900000) { // ldr rL, [rR],#0-#0xFFF
@@ -107,51 +128,32 @@ bool newSlot2Access() {
 		*(u32*)(fixedRegR+exceptionRegisters[opcodeLastByte]) = exceptionRegisters[regL];
 		return true;
 	} else
-	if (opcode == 0x01D00000 && opcodeLastByte >= 0xB0 && opcodeLastByte <= 0xBF) { // ldrh rL, [rR]
+	if ((opcode == 0x01D00000 || opcode == 0x01C00000) && opcodeLastByte >= 0xB0 && opcodeLastByte <= 0xBF) { // ldrh/strh rL, [rR]
 		u8 offsetChange = opcodeLastByte - 0xB0;
 
-		opcodeAlt -= 0x01D00000;
+		opcodeAlt -= opcode;
 
 		while (opcodeAlt >= 0x00000100) {
 			opcodeAlt -= 0x00000100;
 			offsetChange += 0x10;
 		}
 
-		exceptionRegisters[regL] = *(u16*)(fixedRegR+offsetChange);
-		return true;
-	} else
-	if (opcode == 0x01C00000 && opcodeLastByte >= 0xB0 && opcodeLastByte <= 0xBF) { // strh rL, [rR]
-		u8 offsetChange = opcodeLastByte - 0xB0;
-
-		opcodeAlt -= 0x01C00000;
-
-		while (opcodeAlt >= 0x00000100) {
-			opcodeAlt -= 0x00000100;
-			offsetChange += 0x10;
+		if (opcode == 0x01D00000) {
+			exceptionRegisters[regL] = *(u16*)(fixedRegR+offsetChange);
+		} else {
+			*(u16*)(fixedRegR+offsetChange) = (u16)exceptionRegisters[regL];
 		}
-
-		*(u16*)(fixedRegR+offsetChange) = (u16)exceptionRegisters[regL];
 		return true;
 	} else
-	if (opcode == 0x00D00000 && opcodeLastByte >= 0xB0 && opcodeLastByte <= 0xBF) { // ldrh rL, [rR],#0-#0xFF
-		exceptionRegisters[regL] = *(u16*)fixedRegR;
+	if ((opcode == 0x00D00000 || opcode == 0x00C00000) && opcodeLastByte >= 0xB0 && opcodeLastByte <= 0xBF) { // ldrh/strh rL, [rR],#0-#0xFF
+		if (opcode == 0x00D00000) {
+			exceptionRegisters[regL] = *(u16*)fixedRegR;
+		} else {
+			*(u16*)fixedRegR = (u16)exceptionRegisters[regL];
+		}
 		u8 regAddCount = opcodeLastByte - 0xB0;
 
-		opcodeAlt -= 0x00D00000;
-
-		while (opcodeAlt >= 0x00000100) {
-			opcodeAlt -= 0x00000100;
-			regAddCount += 0x10;
-		}
-
-		exceptionRegisters[regR] += regAddCount;
-		return true;
-	} else
-	if (opcode == 0x00C00000 && opcodeLastByte >= 0xB0 && opcodeLastByte <= 0xBF) { // strh rL, [rR],#0-#0xFF
-		*(u16*)fixedRegR = (u16)exceptionRegisters[regL];
-		u8 regAddCount = opcodeLastByte - 0xB0;
-
-		opcodeAlt -= 0x00C00000;
+		opcodeAlt -= opcode;
 
 		while (opcodeAlt >= 0x00000100) {
 			opcodeAlt -= 0x00000100;
@@ -170,9 +172,28 @@ bool newSlot2Access() {
 		exceptionRegisters[regL] = *(u8*)fixedRegR;
 		exceptionRegisters[regR] += (u16)opcode;
 		return true;
+	} else
+	if ((opcode == 0x07D00000) && ((opcodeLastByte >= 0x00 && opcodeLastByte <= 0x0C) || (opcodeLastByte >= 0x80 && opcodeLastByte <= 0x8C))) { // ldrb rL, [rR, r0-r12, lsl #?]
+		u8 lsl = (opcodeLastByte >= 0x80 && opcodeLastByte <= 0x8C) ? 1 : 0;
+
+		opcodeAlt -= opcode;
+
+		while (opcodeAlt >= 0x00000100) {
+			opcodeAlt -= 0x00000100;
+			lsl += 2;
+		}
+
+		u32 add = exceptionRegisters[opcodeLastByte - 0x80];
+		while (lsl > 1) {
+			add += add;
+			lsl--;
+		}
+
+		exceptionRegisters[regL] = *(u8*)(fixedRegR+add);
+		return true;
 	}
 
-	debugArea[1] = 3;
+	// debugArea[1] = 3;
 
 	return false;
 }
