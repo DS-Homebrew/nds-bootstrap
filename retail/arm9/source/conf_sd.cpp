6b6de7052deee1cbda213c703b4b5e2dcc3d9607
RocketRobz <bobesh8@gmail.com>
2026-01-13 21:07:39 -0700

    Force sleep mode to work for EZ-Flash Parallel

--- retail/arm9/source/conf_sd.cpp
+++ retail/arm9/source/conf_sd.cpp
@@ -738,6 +738,7 @@ int loadFromSD(configuration* conf, const char *bootstrapPath) {
 		   (memcmp(io_dldi_data->friendlyName, "Ace3DS+", 7) == 0)
 		|| (memcmp(io_dldi_data->friendlyName, "DEMON", 5) == 0)
 		|| (memcmp(io_dldi_data->friendlyName, "DSTT", 4) == 0)
+		|| (memcmp(io_dldi_data->friendlyName, "EZ-Flash Parallel", 17) == 0)
 		|| (memcmp(io_dldi_data->friendlyName, "R4iDSN", 6) == 0)
 		|| (memcmp(io_dldi_data->friendlyName, "TTCARD", 6) == 0)
 		);
